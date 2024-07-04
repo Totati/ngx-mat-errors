7aafe34d7aabcc266648932d50627a47d3558e03
Totati <attila.th@hotmail.com>
2024-07-04 10:24:52 +0200

    chore: update angular to version 18.0.0

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -24,7 +24,7 @@ import {
   NgxMatErrorDef,
   NgxMatErrorsForDateRangePicker,
 } from 'ngx-mat-errors';
-import { AsyncMinLengthValidator } from './async-validator.directive';
+import { AsyncMinLengthValidator } from './async-min-length-validator.directive';
 import { of, delay } from 'rxjs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
